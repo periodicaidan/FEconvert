d97982304552cb4731e6ce93424eea43ea198b59
Jack Murphy <signal.space@protonmail.com>
2020-12-07 15:20:34 -0600

    s3 upload is working

--- routes/media-api-routes.js
+++ routes/media-api-routes.js
@@ -1,25 +1,20 @@
 // S3 ROUTES
 const upload = require("../services/file-upload");
-// const singleUpload = upload.single('media');
+// const singleUpload = upload.single('media
+const multer = require("multer")
 const db = require("../models");
 
 
 module.exports = function(app) {
 
-    app.post("/api/media_upload", function(req, res) {
+    app.post("/api/media_upload", multer({ dest: 'temp/', limits: { fieldSize: 8 * 1024 * 1024 } }).single(
+        'newMedia'
+    ), function(req, res) {
         if (!req.user) {
             // The user is not logged in, send back an empty object
             res.status(401).json({});
         } else {
-            upload(req, res, function(err, result) {
-                // return res.json({ 'mediaUrl': req.file.location })
-                //TODO
-                // db.Media.create({
-                // mediaName: req.file.name,
-                // mediaLink: req.file.location
-                // });
-                console.log(result);
-            });
+            upload(req, res)
         }
     });
 
--- services/file-upload.js
+++ services/file-upload.js
@@ -35,42 +35,51 @@ const aws = require('aws-sdk');
 const fs = require('fs');
 
 
-const upload = {
-  signup(req, res) {
-    aws.config.setPromisesDependency();
+function upload(req, res) {
+    // aws.config.setPromisesDependency();
+    //console.log(req.file);
+    //console.log(process.env);
     aws.config.update({
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS,
-      region: process.env.REGION
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS,
+        region: process.env.REGION
     });
-    const s3 = new aws.S3();
+    const s3 = new aws.S3({
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS,
+        region: process.env.REGION
+    });
     var params = {
-      ACL: 'public-read',
-      Bucket: process.env.BUCKET_NAME,
-      Body: fs.createReadStream(req.file.path),
-      Key: `userMedia/${req.file.originalname}`
+        ACL: 'public-read',
+        Bucket: process.env.BUCKET_NAME,
+        Body: fs.createReadStream(req.file.path),
+        Key: `${req.user.username}/${req.file.originalname}`
     };
-
-    s3.upload(params, (err, data) => {
-      if (err) {
-        console.log('Error occured while trying to upload to S3 bucket', err);
-      }
-
-      if (data) {
-        fs.unlinkSync(req.file.path); // Empty temp folder
-        const locationUrl = data.Location;
-        let newMedia = db.Media.create({ ...req.body, media: locationUrl });
-        newMedia
-          .save()
-          .then(user => {
-            res.json({ message: 'User created successfully', user });
-          })
-          .catch(err => {
-            console.log('Error occured while trying to save to DB');
-          });
-      }
+    //console.log(s3);
+    s3.putObject(params, (err, data) => {
+        if (err) {
+            console.log('Error occured while trying to upload to S3 bucket', err);
+        }
+        console.log(data);
+        //const objectUrl = s3.getObjectUrl();
+        if (data) {
+            fs.unlinkSync(req.file.path); // Empty temp folder
+            const locationUrl = data.Location;
+            let newMedia = db.Media
+                .create({
+                    // id: req.user.id,
+                    mediaName: req.file.originalname,
+                    mediaLink: "https://summermute-fempeg.s3.us-east-2.amazonaws.com/" + req.user.username + "/" + req.file.originalname,
+                    UserId: req.user.id
+                })
+            	.then(media => {
+                    res.json({ message: 'Media upload success.', media });
+                })
+                .catch(err => {
+                    console.log('Error occured while trying to save to DB');
+                    console.log(err);
+                });
+        }
     });
-  }
 };
-
 module.exports = upload;
